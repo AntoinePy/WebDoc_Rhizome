01faee4cdb16d11a73a039a6a6269725e7afa08a
AntoinePy <guildantoine@hotmail.fr>
2019-04-09 18:07:44 +0200

    Beginning modal

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/* $(document).ready(function(e) {
-    $('img[usemap]').rwdImageMaps();
-}); */
-
 var dialog1;
 var dialog2;
 var audio = document.getElementById("guitare");
@@ -14,22 +10,32 @@ $("#atelier").mapster(
     fillColor: 'ff0000',
     fillOpacity: 0.3
 });
-$("#woman").click(function(){
-    $("#dialog").dialog({
-        classes: {
-            "ui-dialog":"highlight"
-        },
-        modal: true
-    });
-    dialog1 = $("#dialog").dialog();
-});
-
-$("#val1").click(function(){
-        dialog2 = $("#dialog2").dialog();
-        dialog1.dialog('close');
-
+var modal = document.getElementById('myModal');
+var modal2 = document.getElementById('myModal2');
+var femme = document.getElementById('woman');
+var btn1 = document.getElementById('val1');
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+femme.onclick = function(){
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }else if(event.target == modal2){
+        modal2.style.display = "none";
     }
-);
+}
+btn1.onclick = function(){
+    modal.style.display = "none";
+    modal2.style.display = "block";
+}
 
 $("input:checkbox").on('click', function() {
     var $box = $(this);
