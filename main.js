ba0c4b207582f79d26ea3afbe4a887f0cffbf532
AntoinePy <guildantoine@hotmail.fr>
2019-04-09 16:26:12 +0200

    Mapster try

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-
 $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps();
 });
 
-
 var dialog1;
 var dialog2;
 var audio = document.getElementById("guitare");
-
-
+function eventFire(el, etype){
+    console.log("Jacky");
+    if (el.fireEvent) {
+      el.fireEvent('on' + etype);
+    } else {
+      var evObj = document.createEvent('Events');
+      evObj.initEvent(etype, true, false);
+      el.dispatchEvent(evObj);
+    }
+  }
 $("#chitarra").click(function(){
     audio.play();
     return false;
 });
-$("#woman").hover(function() {
-    var pos = $(this).position();
-    console.log(pos);
-    $(this).css(" outline", "solid 10px red");
-});
+var image = $("atelier");
+console.log(image);
+image.mapster(
+{
+        fillOpacity: 0.4,
+        fillColor: "d42e16",
+        stroke: true,
+        strokeColor: "3320FF",
+        strokeOpacity: 0.8,
+        strokeWidth: 4,
+        singleSelect: true,
+        mapKey: 'name',
+        listKey: 'name'
+}   
+);
 $("#woman").click(function(){
     $("#dialog").dialog({
         classes: {
