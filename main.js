d4ae4b808e4a17c789db25dcd20293b3a74fd420
AntoinePy <guildantoine@hotmail.fr>
2019-04-09 16:44:21 +0200

    Mapster hover femme

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
-$(document).ready(function(e) {
+/* $(document).ready(function(e) {
     $('img[usemap]').rwdImageMaps();
-});
+}); */
 
 var dialog1;
 var dialog2;
 var audio = document.getElementById("guitare");
-function eventFire(el, etype){
-    console.log("Jacky");
-    if (el.fireEvent) {
-      el.fireEvent('on' + etype);
-    } else {
-      var evObj = document.createEvent('Events');
-      evObj.initEvent(etype, true, false);
-      el.dispatchEvent(evObj);
-    }
-  }
 $("#chitarra").click(function(){
     audio.play();
     return false;
 });
-var image = $("atelier");
-console.log(image);
-image.mapster(
+$("#atelier").mapster(
 {
-        fillOpacity: 0.4,
-        fillColor: "d42e16",
-        stroke: true,
-        strokeColor: "3320FF",
-        strokeOpacity: 0.8,
-        strokeWidth: 4,
-        singleSelect: true,
-        mapKey: 'name',
-        listKey: 'name'
-}   
-);
+    fillColor: 'ff0000',
+    fillOpacity: 0.3
+});
 $("#woman").click(function(){
     $("#dialog").dialog({
         classes: {
